eedc7fd0f7caa2aa4c7e8dcb7b507072f09d66cf
Thomas <thomas.whyyou@gmail.com>
2023-05-10 01:30:09 -0400

    update

--- Formula/groot.rb
+++ Formula/groot.rb
@@ -7,6 +7,8 @@ class Groot < Formula
   desc "I am Groot!"
   homepage "https://github.com/knocklabs/knock-groot"
   url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.4/groot-v0.0.4-1bb504f-darwin-x64.tar.gz"
+  sha256 "8a77673c95dc566af5f3ad846589c5c6166d5dfcaa1b803e992da034cf9fa73e"
+  license ""
 
   depends_on "node"
 
