44806c490fd43ebd076085e21872376a00240893
Thomas <thomas.whyyou@gmail.com>
2023-05-10 02:11:29 -0400

    update

--- Formula/groot.rb
+++ Formula/groot.rb
@@ -1,16 +1,12 @@
 # Reference:
 # https://github.com/twilio/homebrew-brew/blob/main/Formula/twilio.rb
 
-# require "language/node"
-
 class Groot < Formula
   desc "I am Groot!"
   homepage "https://github.com/knocklabs/knock-groot"
   url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.4/groot-v0.0.4-1bb504f-darwin-x64.tar.gz"
   sha256 "8a77673c95dc566af5f3ad846589c5c6166d5dfcaa1b803e992da034cf9fa73e"
   license ""
-
-  # depends_on "node"
 
   def install
     inreplace "bin/groot", /^CLIENT_HOME=/, "export GROOT_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
