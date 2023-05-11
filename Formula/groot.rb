a6ab3ccec6adaf2b104102e24a468b2f0d98b663
Thomas <thomas.whyyou@gmail.com>
2023-05-11 20:01:50 +0100

    groot 0.0.13
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/groot.rb
+++ Formula/groot.rb
@@ -4,8 +4,8 @@
 class Groot < Formula
   desc "I am Groot!"
   homepage "https://github.com/knocklabs/knock-groot"
-  url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.5/groot-v0.0.5-darwin-x64.tar.gz"
-  sha256 "38bb137bce626f85934f62fdd5c976a3f9b5a044568f4f5d269e8a3272be46d7"
+  url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.13/groot-v0.0.13-cded107-darwin-x64.tar.gz"
+  sha256 "267a5e8a5bab24fb5e1aee65280908f94a43efc88249e748ece18adfc4545bde"
   license ""
 
   bottle do
