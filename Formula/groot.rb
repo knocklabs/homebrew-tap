8b5c73376ec5deaa05a223c62b9ccc6ccebad54d
Thomas <thomas.whyyou@gmail.com>
2023-05-12 20:32:35 +0100

    release: groot 0.0.50
    This PR is opened and managed automatically by Github Actions.
    
    formula: groot
    version: 0.0.50

--- Formula/groot.rb
+++ Formula/groot.rb
@@ -4,9 +4,9 @@
 class Groot < Formula
   desc "I am Groot!"
   homepage "https://github.com/knocklabs/knock-groot"
-  url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.49/groot-v0.0.49-b7f843f-darwin-x64.tar.gz"
-  version "0.0.49"
-  sha256 "8c79875f656528345c641768c448bea625e5ebd5c8b5901f3937b9335b68709c"
+  url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.50/groot-v0.0.50-e98e096-darwin-x64.tar.gz"
+  version "0.0.50"
+  sha256 "f606af0e6baf85f820d76b2ebf4384c4240ed44de9c1b07a8d112e3d7cf35b41"
 
   def install
     inreplace "bin/groot", /^CLIENT_HOME=/, "export GROOT_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
