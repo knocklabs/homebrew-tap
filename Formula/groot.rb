850602e5f4fe770488c7c781a8b9b72ac6e7f1d8
Thomas <thomas.whyyou@gmail.com>
2023-05-12 12:54:45 -0400

    update

--- Formula/groot.rb
+++ Formula/groot.rb
@@ -5,8 +5,8 @@ class Groot < Formula
   desc "I am Groot!"
   homepage "https://github.com/knocklabs/knock-groot"
   url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.40/groot-v0.0.40-7f9715e-darwin-x64.tar.gz"
-  sha256 "056ec1d01caf1c694dc3df1ffa9c2cc27b653e155844c22113b08233241870d9"
   version "0.0.40"
+  sha256 "056ec1d01caf1c694dc3df1ffa9c2cc27b653e155844c22113b08233241870d9"
 
   def install
     inreplace "bin/groot", /^CLIENT_HOME=/, "export GROOT_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
