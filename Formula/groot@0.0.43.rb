1a6c2c9c0174934f19e1e840297469134d09087f
Thomas <thomas.whyyou@gmail.com>
2023-05-12 18:16:48 +0000

    groot 0.0.43

---
+++ Formula/groot@0.0.43.rb
@@ -0,0 +1,20 @@
+# Reference:
+# https://github.com/twilio/homebrew-brew/blob/main/Formula/twilio.rb
+
+class GrootAT0043 < Formula
+  desc "I am Groot!"
+  homepage "https://github.com/knocklabs/knock-groot"
+  url "https://github.com/knocklabs/knock-groot/releases/download/v0.0.43/groot-v0.0.43-e2b2921-darwin-x64.tar.gz"
+  version "0.0.43"
+  sha256 "30fa398191cec08bf1e4742b1f082fb5bb7c0d4329c8dab22897bfc7157da7d2"
+
+  def install
+    inreplace "bin/groot", /^CLIENT_HOME=/, "export GROOT_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/groot"
+  end
+
+  test do
+    system bin/"groot", "--version"
+  end
+end
