1faa7ade387eb898d7ecaa643df9472d5a20c48d
Chris Bell <cjbell1988@gmail.com>
2026-08-13 11:20:26 -0400

    chore: fix 1.2.2

---
+++ Formula/knock@1.2.2.rb
@@ -0,0 +1,20 @@
+# Reference:
+# https://github.com/twilio/homebrew-brew/blob/main/Formula/twilio.rb
+
+class KnockAT121 < Formula
+  desc "Build and manage your notifications with Knock directly from the command-line"
+  homepage "https://github.com/knocklabs/knock-cli"
+  url "https://github.com/knocklabs/knock-cli/releases/download/v1.2.2/knock-v1.2.2-cfe0822-darwin-x64.tar.gz"
+  version "1.2.2"
+  sha256 "f644d551fd2be3e1ac5b9b6c5489180365403a91f61309948cc80b264fc3394b"
+
+  def install
+    inreplace "bin/knock", /^CLIENT_HOME=/, "export KNOCK_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/knock"
+  end
+
+  test do
+    system bin/"knock", "--version"
+  end
+end
